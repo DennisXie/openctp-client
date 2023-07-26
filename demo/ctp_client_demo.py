96bafd0cd7e2a14aaf5ef0f682fd71cf5386d02e
dennis xie <dennisxie1992@outlook.com>
2023-07-26 21:36:39 +0800

    Move md_client to md_client.py and remove multi callback support

--- demo/ctp_client_demo.py
+++ demo/ctp_client_demo.py
@@ -1,6 +1,6 @@
 from openctp_client.objects import *
 from openctp_client.objects.enums import CtpMethod
-from openctp_client.clients.simple_ctp_client import MdClient
+from openctp_client.clients.md_client import MdClient
 
 if __name__ == "__main__":
     config = CtpConfig("tcp://180.168.146.187:10211", "9999", "9999", "9999", "9999", "9999")
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     def on_market_data(data: DepthMarketDataField):
         print(f"market data: {data.InstrumentID} {data.AskPrice1}")
     
-    client.add_spi_callback(CtpMethod.OnRspUserLogin, connected)
-    client.add_spi_callback(CtpMethod.OnRspSubMarketData, on_subscribe_market_data)
-    client.add_spi_callback(CtpMethod.OnRtnDepthMarketData, on_market_data)
+    client.set_spi_callback(CtpMethod.OnRspUserLogin, connected)
+    client.set_spi_callback(CtpMethod.OnRspSubMarketData, on_subscribe_market_data)
+    client.set_spi_callback(CtpMethod.OnRtnDepthMarketData, on_market_data)
     
     client.Connect()
     
--- tests/clients/test_md_client.py
+++ tests/clients/test_md_client.py
@@ -2,7 +2,7 @@ import pytest
 from pytest_mock import MockerFixture
 from openctp_ctp import mdapi
 
-from openctp_client.clients.simple_ctp_client import MdClient
+from openctp_client.clients.md_client import MdClient
 from openctp_client.objects import *
 
 
@@ -17,26 +17,25 @@ def config():
 
 @pytest.fixture
 def client(config: CtpConfig, mocker: MockerFixture):
-    mdapi = mocker.patch("openctp_client.clients.simple_ctp_client.mdapi")
+    mdapi = mocker.patch("openctp_client.clients.md_client.mdapi")
     api = mocker.Mock(name="api")
     mdapi.CThostFtdcMdApi.CreateFtdcMdApi.return_value = api
 
     return MdClient(config)
 
 
-def test_should_get_spi_callback_when_add_spi_callback_to_md_client(config: CtpConfig, spi_callback):
+def test_should_get_spi_callback_when_set_spi_callback_to_md_client(config: CtpConfig, spi_callback):
     client = MdClient(config)
-    client.add_spi_callback(CtpMethod.OnOrderInsert, spi_callback)
-    callbacks = client.get_spi_callback(CtpMethod.OnOrderInsert)
-    assert len(callbacks) == 1
-    assert callbacks[0] == spi_callback
+    client.set_spi_callback(CtpMethod.OnOrderInsert, spi_callback)
+    callback = client.get_spi_callback(CtpMethod.OnOrderInsert)
+    assert callback == spi_callback
 
 def test_should_get_none_when_del_spi_callback_from_md_client(config: CtpConfig, spi_callback):
     client = MdClient(config)
-    client.add_spi_callback(CtpMethod.OnOrderInsert, spi_callback)
+    client.set_spi_callback(CtpMethod.OnOrderInsert, spi_callback)
     client.del_spi_callback(CtpMethod.OnOrderInsert, spi_callback)
-    callbacks = client.get_spi_callback(CtpMethod.OnOrderInsert)
-    assert len(callbacks) == 0
+    callback = client.get_spi_callback(CtpMethod.OnOrderInsert)
+    assert callback is None
 
 # The fixture seems called by the order of they are required by the test function
 def test_should_call_Init_when_Connect(client):
@@ -49,7 +48,7 @@ def test_should_call_api_ReqUserLogin_when_OnFrontConnected(client):
 
 def test_should_call_callback_when_OnRspUserLogin(client, spi_callback):
     # given
-    client.add_spi_callback(CtpMethod.OnRspUserLogin, spi_callback)
+    client.set_spi_callback(CtpMethod.OnRspUserLogin, spi_callback)
     pRspUserLogin = mdapi.CThostFtdcRspUserLoginField()
     pRspInfo = mdapi.CThostFtdcRspInfoField()
     # when
@@ -63,7 +62,7 @@ def test_should_call_api_SubscribeMarketData_when_SubscribeMarketData(client):
 
 def test_should_call_callback_when_OnRspSubMarketData(client, spi_callback):
     # given
-    client.add_spi_callback(CtpMethod.OnRspSubMarketData, spi_callback)
+    client.set_spi_callback(CtpMethod.OnRspSubMarketData, spi_callback)
     pSpecificInstrument = mdapi.CThostFtdcSpecificInstrumentField()
     pRspInfo = mdapi.CThostFtdcRspInfoField()
     # when
@@ -73,7 +72,7 @@ def test_should_call_callback_when_OnRspSubMarketData(client, spi_callback):
 
 def test_should_call_callback_when_OnRtnDepthMarketData(client, spi_callback):
     # given
-    client.add_spi_callback(CtpMethod.OnRtnDepthMarketData, spi_callback)
+    client.set_spi_callback(CtpMethod.OnRtnDepthMarketData, spi_callback)
     pDepthMarketData = mdapi.CThostFtdcDepthMarketDataField()
     # when
     client.OnRtnDepthMarketData(pDepthMarketData)
