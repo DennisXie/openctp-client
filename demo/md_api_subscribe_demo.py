2f17863c7b5fd75a1038e055873279b6fb2ddf2e
dennis xie <dennisxie1992@outlook.com>
2023-08-03 22:25:02 +0800

    fix the demo

--- demo/md_api_subscribe_demo.py
+++ demo/md_api_subscribe_demo.py
@@ -3,23 +3,23 @@ from openctp_client.objects.enums import CtpMethod
 from openctp_client.apis.md_api import MdAPI
 
 if __name__ == "__main__":
-    config = CtpConfig("tcp://180.168.146.187:10211", "9999", "9999", "9999", "9999", "9999")
-    client = MdAPI(config)
+    config = CtpConfig("", "tcp://180.168.146.187:10211", "9999", "9999", "9999", "9999", "9999")
+    md_api = MdAPI(config)
     
     def connected(login_info: RspUserLoginField, rsp_info: RspInfoField, request_id: int, last: bool):
         print(f"connected: {login_info.TradingDay}")
-        client.SubscribeMarketData(["ag2308"])
+        md_api.SubscribeMarketData(["ag2308"])
     
     def on_subscribe_market_data(instrument: SpecificInstrumentField, rsp_info: RspInfoField, request_id: int, last: bool):
-        print(f"subscribe market data: {instrument.InstrumentID}")
+        print(f"subscribe market data: {instrument.model_dump_json()}")
     
     def on_market_data(data: DepthMarketDataField):
-        print(f"market data: {data.InstrumentID} {data.AskPrice1}")
+        print(f"market data: {data.model_dump_json()}")
     
-    client.set_spi_callback(CtpMethod.OnRspUserLogin, connected)
-    client.set_spi_callback(CtpMethod.OnRspSubMarketData, on_subscribe_market_data)
-    client.set_spi_callback(CtpMethod.OnRtnDepthMarketData, on_market_data)
+    md_api.set_spi_callback(CtpMethod.OnRspUserLogin, connected)
+    md_api.set_spi_callback(CtpMethod.OnRspSubMarketData, on_subscribe_market_data)
+    md_api.set_spi_callback(CtpMethod.OnRtnDepthMarketData, on_market_data)
     
-    client.Connect()
+    md_api.Connect()
     
     ch = input("press any key to exit")
