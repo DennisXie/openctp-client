3d375f98dbf480efaefd15570780c7090ad0199e
dennis xie <dennisxie1992@outlook.com>
2023-08-04 22:44:26 +0800

    Add settlement confirm logic

--- demo/query_settlement_info.py
+++ demo/query_settlement_info.py
@@ -6,6 +6,7 @@ from openctp_client.objects.enums import CtpMethod
 from openctp_client.apis.td_api import TdAPI
 
 connected_event = threading.Event()
+settlement_event = threading.Event()
 
 def connected(login_info: RspUserLoginField, rsp_info: RspInfoField, request_id: int, is_last: bool):
     print("connected")
@@ -16,6 +17,14 @@ def on_settlement_info(settlement_info: SettlementInfoField, rsp_info: RspInfoFi
         print(f"error {rsp_info.model_dump()}")
     if settlement_info:
         print(settlement_info.Content)
+    if is_last:
+        settlement_event.set()
+
+def on_settlement_confirm(settlement_info_confirm: SettlementInfoConfirmField, rsp_info: RspInfoField, request_id: int, is_last: bool):
+    if rsp_info:
+        print(f"settlement confirm error {rsp_info.model_dump()}")
+    if settlement_info_confirm:
+        print(settlement_info_confirm.model_dump())    
 
 
 config = CtpConfig(
@@ -31,6 +40,7 @@ config = CtpConfig(
 td_api = TdAPI(config=config)
 td_api.set_spi_callback(CtpMethod.OnRspUserLogin, connected)
 td_api.set_spi_callback(CtpMethod.OnRspQrySettlementInfo, on_settlement_info)
+td_api.set_spi_callback(CtpMethod.OnRspQrySettlementInfoConfirm, on_settlement_confirm)
 
 td_api.Connect()
 connected_event.wait()
@@ -41,4 +51,12 @@ qry_settlement_info = QrySettlementInfoField(
     TradingDay="20230803",
 )
 td_api.ReqQrySettlementInfo(qry_settlement_info)
+settlement_event.wait()
+ch = input("press to confirm settlement info\n")
+settlement_info_confirm = QrySettlementInfoConfirmField(
+    BrokerID=config.broker_id,
+    InvestorID=config.user_id,
+    AccountID=config.user_id,
+)
+td_api.ReqQrySettlementInfoConfirm(settlement_info_confirm)
 ch = input("press any key to exit\n")
